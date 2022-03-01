9c3924f5053e567bbad0e596bd02180ce10ae3f6
Your Name <shauryasinghal39@gmail.com>
2022-03-01 16:43:53 +0530

    Package Sorted

---
+++ allPackage.py
@@ -0,0 +1,18 @@
+import pyttsx3
+from gtts import gTTS
+from playsound import playsound
+import speech_recognition as sr
+import cv2
+import mediapipe as mp
+from math import hypot
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+import numpy as np
+import pywhatkit
+import wikipedia
+from pywikihow import WikiHow, search_wikihow
+import os
+import sys
+from icrawler.builtin import GoogleImageCrawler
+import webbrowser as web
